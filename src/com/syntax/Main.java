7a6787ed3281037ac37b8615783ce2f3dcf10a34
Medine Kaya <99620226+karojjj@users.noreply.github.com>
2022-06-21 21:43:35 -0400

    2nd commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code here
         for (int i=0; i<5; i++){
             System.out.println("hello! enough for coding!");
+            System.out.println("something");
         }
     }
 }
