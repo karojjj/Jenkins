d3f0fcaf4b0b7f7e71c77ff211c35edbdccf0ae7
Medine Kaya <99620226+karojjj@users.noreply.github.com>
2022-06-21 21:08:30 -0400

    1th commit

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,11 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i=0; i<5; i++){
+            System.out.println("hello! enough for coding!");
+        }
+    }
+}
